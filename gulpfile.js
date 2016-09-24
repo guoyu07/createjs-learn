1f8c5415936fb9d9b86d9d4ee1b97c3abedd4e9e
Ivan Jobs <ivan1377@163.com>
2016-09-24 12:35:53 +0800

    refine

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var webserver = require('gulp-webserver');
 gulp.task('default', function(){
     gulp.src('src')
         .pipe(webserver({
+	    host: '192.168.1.234',
             livereload: true,
             open: true,
             directoryListing: false
